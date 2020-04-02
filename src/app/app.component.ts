425f5a95275a3b6d6b31ba1d4ba869db543095da
johncolecarter <colecarter7777@gmail.com>
2020-04-01 20:18:27 -0500

    added addTodo Function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,17 @@ export class AppComponent implements OnInit {
     ];
   }
 
+  // adds a todo to our list
+  addTodo(): void {
+    this.todoList.push({
+      title: this.todoTitle,
+      isDone: false
+    });
+
+    // resets our todoTitle variable to an empty string
+    this.todoTitle = '';
+  }
+
   deleteTodo(todo: any) {
     const index = this.todoList.findIndex(todoItem => todoItem === todo);
     this.todoList.splice(index, 1);
