fd41631c1a5aa3b10bb2c20b560485d2be0c5864
johncolecarter <colecarter7777@gmail.com>
2020-04-01 19:44:18 -0500

    testing Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'NG421wk1day1ToDoApp';
+  title = "Todos";
+  todoList: any[] = [];
 }
