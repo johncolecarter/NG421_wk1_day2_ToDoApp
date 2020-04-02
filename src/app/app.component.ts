ffbda65cb88ed310c98a84e565919658ab9b0806
johncolecarter <colecarter7777@gmail.com>
2020-04-01 19:55:42 -0500

    Added Delete Function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = "Todos";
+export class AppComponent implements OnInit {
+  title = 'Todos';
   todoList: any[] = [];
+
+  ngOnInit() {
+    this.todoList = [
+      // example of how to make an item in todo list
+      { title: 'Install Angular CLI', isDone: false }
+    ];
+  }
+
+  deleteTodo(todo: any) {
+    const index = this.todoList.findIndex(todoItem => todoItem === todo);
+    this.todoList.splice(index, 1);
+  }
 }
